a6f0c2e08120491582dc1c00dbf932131e140a89
2ndborn <>
2024-02-01 20:54:55 +0000

    corrected js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -56,25 +56,25 @@ form.addEventListener('submit', function (ev) {
 
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
     // From using {% csrf_token %} in the form
-    var csrfToken = $('input["name=csrfmiddlewaretoken"]').val();
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
-    }
+    };
     var url = '/checkout/cache_checkout_data/';
 
-    $.post(url, postData).done(function() {
+    $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
                 billing_details: {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
-                    name: $.trim(form.email.value),
+                    email: $.trim(form.email.value),
                     address: {
                         line1: $.trim(form.street_address1.value),
-                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
@@ -84,10 +84,9 @@ form.addEventListener('submit', function (ev) {
             shipping: {
                 name: $.trim(form.full_name.value),
                 phone: $.trim(form.phone_number.value),
-                name: $.trim(form.email.value),
                 address: {
                     line1: $.trim(form.street_address1.value),
-                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     postal_code: $.trim(form.postcode.value),
@@ -98,10 +97,10 @@ form.addEventListener('submit', function (ev) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
                 var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
                 $('#payment-form').fadeToggle(100);
                 $('#loading-overlay').fadeToggle(100);
@@ -113,8 +112,8 @@ form.addEventListener('submit', function (ev) {
                 }
             }
         });
-    }).fail(function() {
-        // Just reload the page, the error will be in django messages
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
